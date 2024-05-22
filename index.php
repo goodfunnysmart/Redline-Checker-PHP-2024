a99e9e5ab92e8e1337bfd23bc3ea39846449e6ba
goodfunnysmart <131162558+goodfunnysmart@users.noreply.github.com>
2024-05-22 07:19:56 +0000

    Working Green/Red

--- index.php
+++ index.php
@@ -24,10 +24,12 @@ function calculateEMA($data, $period = 65) {
 
 $stocks = ['AZJ.AX', 'BHP.AX', 'GDX.AX', 'HVN.AX', 'MYR.AX', 'NST.AX', 'PSC.AX', 'RIO.AX', 'STO.AX', 'WBC.AX', 'WES.AX'];
 
-echo "<html><head><style>
+$emailContent = "<html><head><style>
     .green { color: green; }
     .red { color: red; }
 </style></head><body><h1>Australian Stock Status</h1><ul>";
+
+$sendEmail = false;
 
 foreach ($stocks as $stock) {
     try {
@@ -51,14 +53,63 @@ foreach ($stocks as $stock) {
         $formattedEma = '$' . number_format((float)$latestEma, 2, '.', '');
 
         if ($latestClose < $latestEma) {
+            $emailContent .= "<li class='red'>$stock: Close = $formattedClose, 65-day EMA = $formattedEma</li>";
+            $sendEmail = true;
             echo "<li class='red'>$stock: Close = $formattedClose, 65-day EMA = $formattedEma</li>";
         } else {
+            $emailContent .= "<li class='green'>$stock: Close = $formattedClose, 65-day EMA = $formattedEma</li>";
             echo "<li class='green'>$stock: Close = $formattedClose, 65-day EMA = $formattedEma</li>";
         }
     } catch (Exception $e) {
+        $emailContent .= "<li class='red'>$stock: Error - " . $e->getMessage() . "</li>";
         echo "<li class='red'>$stock: Error - " . $e->getMessage() . "</li>";
     }
 }
 
-echo "</ul></body></html>";
+$emailContent .= "</ul></body></html>";
+
+if ($sendEmail) {
+    $to = "david@goodfunnysmart.com";
+    $subject = "Stock Alert";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: mail@greache.com' . "\r\n";
+
+    mail($to, $subject, $emailContent, $headers);
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Stock Status</title>
+    <style>
+        .green { color: green; }
+        .red { color: red; }
+        #spinner {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 9999;
+        }
+        .spinner-border {
+            width: 4rem;
+            height: 4rem;
+        }
+    </style>
+    <script>
+        window.onload = function() {
+            document.getElementById('spinner').style.display = 'none';
+        };
+    </script>
+</head>
+<body>
+<div id="spinner">
+    <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+</body>
+</html>
